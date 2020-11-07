5d6bc0f61a82343e2315ed2a9a7ec7e124ae62ca
Nico Brühlmann <nico@mindshaped.com>
2020-11-07 18:05:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -26,6 +26,10 @@ module.exports = {
             key: 'Cache-Control',
             value: 'public, max-age=2592000', // 30 days
           },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ],
       },
     ]
